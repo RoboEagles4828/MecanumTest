58ea300d1e7ad8416d58eb6873c3f8919919a8c5
Jackie Chen <cheezballz3.14@gmail.com>
2019-01-12 14:15:53 -0500

    Drivetrain Skeleton

---
+++ src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+class DriveTrain {
+    TalonSRX frontLeft, frontRight, backLeft, backRight;
+
+    public DriveTrain(int frontLeft, int frontRight, int backLeft, int backRight) {
+        this.frontLeft = new TalonSRX(frontLeft);
+        // TODO: Do the same for the rest of the motors
+    }
+
+    public int[] math(double x, double y, double t) {
+        int[] output = {0,0,0,0};
+
+        // TODO: The math for this is:
+        // y is added directly to all 4 motors.
+        // x is transferred negatively to the front two motors and additively to the back two.
+        // t (twist) is tranferred additively to the left 2 motors and negatively to the right two motors
+        // After doing all this crap normalize everything to 0-1.
+
+        return output;
+    }
+}
